951f41bf5a762bb31dbf2e34a028e39871b36993
Abhijat <abhijat@krypton.local>
2012-01-28 19:02:38 +0530

    Added signal management

--- triggers.py
+++ triggers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, signal, time
+import os, signal, time, sys
 from subprocess import *
 
 class trigger:
@@ -13,22 +13,51 @@ class trigger:
         self.trigger_state = 'N'
         self.load = 0.0
 
-    def signal_handler(signal_number):
-        pass
+    def signal_handler(self, signal_number, stack_frame):
+        try:
+            self.file_handle.close()
+        except:
+            pass
+        if signal_number == 1:
+            sys.exit(0)
 
     def run_pipe(self):
-        pass
+        proc_lst = []
+        for idx,command in enumerate(self.commands):
+            lst = command.split()
+            if idx == 0:
+                proc = Popen(lst, stdout=PIPE, stdin=PIPE)
+            else:
+                proc = Popen(lst, stdin=proc_lst[idx-1].stdout, \
+                        stdout=PIPE, stderr=PIPE)
+            proc_lst.append(proc)
+        (out, err) = proc_lst[len(proc_lst)-1].communicate()    
+        return (out, err)
 
     def start_log(self):
         print 'Starting logging process at load: %f' % self.load
         self.trigger_state = 'Y'
+        child = os.fork()
+
+        if child == 0:
+            self.file_handle = open(self.log_loc, 'wb')
+            while True:
+                signal.signal(1, self.signal_handler)
+                (out, err) = self.run_pipe()
+                self.file_handle.write('%s\n' % out)
+                time.sleep(self.interval)
+        else:
+            self.child_pid = child
 
     def kill_log(self):
         print 'Stopping logging process at load %f' % self.load
         self.trigger_state = 'N'
+        if self.child_pid:
+            killer_proc = Popen(['kill','-1',str(self.child_pid)])
+            killer_proc.communicate()
 
 class cpu_trigger(trigger):
-    def __init__(self, threshold=0.90, interval=5.0):
+    def __init__(self, threshold=0.95, interval=5.0):
         trigger.__init__(self, threshold, interval, check_fn=self.cpu_check)
         self.commands = [
                 'ps -eo pcpu,pid,user,args',
@@ -37,6 +66,7 @@ class cpu_trigger(trigger):
                 ]
         self.test_command = ['uptime']
         self.text = 'CPU Load'
+        self.log_loc = 'cpu_log.txt'
         
     def cpu_check(self, debug_mode='N'):
         proc = Popen(self.test_command,stdout=PIPE,stderr=PIPE)
@@ -57,6 +87,7 @@ def main():
     trig1 = cpu_trigger()
     while True:
         trig1.check_fn('Y')
+        trig1.run_pipe()
         time.sleep(3)
 
 if __name__ == '__main__':
